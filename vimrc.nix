f24323286b42566ff831981acaf16407aced4076
Vladimir Kirillov <proger@wilab.org.ua>
2014-08-09 12:06:19 +0300

    sync

--- vimrc.nix
+++ vimrc.nix
@@ -2,6 +2,7 @@
 
 augroup filetype
 	au FileType nix setlocal expandtab tabstop=2 shiftwidth=2 softtabstop=2 cindent
+	au FileType nix setlocal iskeyword=@,48-57,_,192-255,_,$,@,%,#,-
 augroup END
 
 " vim: filetype=vim
